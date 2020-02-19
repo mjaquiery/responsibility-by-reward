4a502c52709eb8ab851952f5022e387ba9a257f6
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-02-19 12:46:40 +0000

    Simulated new design
    Changed the simulation to use the new design, which is simplified to a 2x2 ANOVA of outcome valence (good, bad) x beneficiary (participant, other).
    
    Took basic responsibility distributions from the 5-member group data to reflect the approach we aim to take in the experiment.

--- simulation/generate-data.R
+++ simulation/generate-data.R
@@ -5,6 +5,8 @@
 #' means are high or low.
 #'
 #' @param n number of participants to simulate for
+#' @param beneficiaryEffectMean mean effect size (in scale points)
+#' @param beneficiaryEffectSD effect size standard deviation (in scale points)
 #' @param effectSizeMean mean effect size (in scale points)
 #' @param effectSizeSD effect size standard deviation (in scale points)
 #' @param nReps number of trials in each condition by each participant
@@ -12,8 +14,10 @@
 #' @return tibble with n x length(nReps) x 4 rows
 generateData <- function(
   n, 
+  beneficiaryEffectMean = 5,
+  beneficiaryEffectSD = 15,
   effectSizeMean = 5, 
-  effectSizeSD = 15, 
+  effectSizeSD = 15,
   nReps = 10
   ) {
   # Experimental conditions with fixed effects
@@ -30,52 +34,59 @@ generateData <- function(
   
   # Participant parameters
   # These values are estimated from empirical data for previous experiments.
+  # Specifically these are taken from the data with group = 'five'
   ps <- tibble(
     pid = 1:N$p,
-    scaleResp_m = rnorm(N$p, 40, 18),
-    scaleResp_sd = abs(rnorm(N$p, 10, 3)),
-    outcome = rnorm(N$p, 3, 1),
-    outcome_sd = abs(rnorm(N$p, 1, 1)),
-    pGetsOutcome = rnorm(N$p, effectSizeMean, effectSizeSD),
+    scaleResp_m = rnorm(N$p, 43, 21),
+    scaleResp_sd = abs(rnorm(N$p, 7, 9)),
+    outcome_m = rnorm(N$p, 11, 23),
+    outcome_sd = abs(rnorm(N$p, 2, 10)),
+    beneficiary = rnorm(N$p, beneficiaryEffectMean, beneficiaryEffectSD),
     # Guestimate a sensible standard deviation roughly based on effect size
-    pGetsOutcome_sd = abs(rnorm(N$p, effectSizeSD, effectSizeSD / 3))
+    beneficiary_sd = abs(rnorm(N$p, beneficiaryEffectSD, beneficiaryEffectSD / 3)),
+    effectSize_m = rnorm(N$p, effectSizeMean, effectSizeSD),
+    # Guestimate a sensible standard deviation roughly based on effect size
+    effectSize_sd = abs(rnorm(N$p, effectSizeSD, effectSizeSD / 3))
   )
   
   # Blank data frame with crossed condition structure
-  data <- tibble(
-    pid = rep(rep(1:N$p, N$r), N$outcomes * N$pGets),
-    trial = rep(1:(N$r * N$outcomes * N$pGets), each = N$p),
-    outcome = rep(rep(outcomes, each = N$p), N$pGets * N$r),
-    pGetsOutcome = rep(rep(pGetsOutcome, each = N$outcomes), each = N$p * N$r),
-    scaleResp = NA,
-    scaleRespFinal = NA
-  )
-  
+  data <- crossing(pid = 1:N$p, 
+                   rep = 1:N$r, 
+                   outcome = outcomes, 
+                   pGetsOutcome = pGetsOutcome) %>%
+    select(-rep) %>%
+    nest(data = -pid) %>%
+    mutate(
+      data = map(data, ~ rowid_to_column(., var = 'trialId'))
+    ) %>%
+    unnest(data) %>%
+    mutate(
+      scaleResp = NA,
+      scaleResp_plain = NA
+    )
+
   # Temporarily join the dataframes to make mutation easier
   data_generator <- left_join(data, ps, by = c('pid'), suffix = c("", ".y"))
   
   # Use participant data to fill in data frame
   data_generator <- data_generator %>%
-    mutate(
-      scaleResp = rnorm(nrow(data_generator), scaleResp_m, scaleResp_sd)
-    ) %>%
     # apply effects
     mutate(
-      # the outcome effect is directional - responsibility is reduced for bad
-      outcome.y = if_else(outcome == "bad", outcome.y * -1, outcome.y),
-      scaleRespFinal = scaleResp + 
-        rnorm(nrow(data_generator), outcome.y, outcome_sd) + 
-        # the pGetsOutcome effect only appears when pGetsOutcome
-        (rnorm(nrow(data_generator), pGetsOutcome.y, pGetsOutcome_sd) *
-        pGetsOutcome)
+      scaleResp = rnorm(n(), scaleResp_m, scaleResp_sd) +                              # basic mean
+        rnorm(n(), outcome_m, outcome_sd) * (outcome == 'good') +                      # outcome valence
+        rnorm(n(), beneficiary, beneficiary_sd) * pGetsOutcome +                       # participant is beneficiary
+        rnorm(n(), effectSize_m, effectSize_sd) * pGetsOutcome * (outcome == 'good'),  # interaction
+      # plain uses static effect sizes for each participant rather than a distribution per trial
+      scaleResp_plain = rnorm(n(), scaleResp_m, scaleResp_sd) +
+        outcome_m * (outcome == 'good') +
+        beneficiary * pGetsOutcome +
+        effectSize_m * pGetsOutcome * (outcome == 'good')
     ) 
   
   # Clamp values to scale limits
   data_generator <- data_generator %>%
-    mutate(
-      scaleResp = pmin(pmax(scaleResp, 0), 100),
-      scaleRespFinal = pmin(pmax(scaleRespFinal, 0), 100)
-    )
+    mutate(scaleResp = pmin(pmax(scaleResp, 0), 100),
+           scaleResp_plain = pmin(pmax(scaleResp_plain, 0), 100))
   
   # Return without the ps fields
   data_generator %>% select(names(data))
