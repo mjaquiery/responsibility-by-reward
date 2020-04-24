5aa434351b4f241575d1b24c719669adb449be83
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-04-24 11:47:02 +0100

    Removed sensitive info

--- ATTRRESP/database_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $servername = "mysql-server.ucl.ac.uk";
-  $port = 3306;
-  $username = "ucjuelz_user";
-  $password = '8282$Kosmos7';
-  $dbname = "ucjuelz";
-  $table = "respexp";
-?>
--- ATTRRESP/database_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//$dbc = mysql_connect('localhost', 'username', 'password');
-//$dbc = mysql_connect('localhost', 'wampp', 'xampp');
-$dbc = mysql_connect('mysql-server.ucl.ac.uk', 'ucjuelz_user', '8282$Kosmos7');
-mysql_select_db('ucjuelz', $dbc);
-
-?>
